d1d496392207c9b5f0c4ab158a9d6a970cca80d4
r4gi <r4gi.github@gmail.com>
2026-03-10 21:39:14 +0900

    Remove repair kit recipe from config
    Removed the repair kit recipe from the configuration.

--- config/recipes.lua
+++ config/recipes.lua
@@ -2,21 +2,6 @@ Config = Config or {}
 
 Config.Recipes = {
 
-    repairkit = {
-        label = "Repair Kit",
-        item = "repairkit",
-        amount = 1,
-
-        materials = {
-            iron = 5,
-            rubber = 2
-        },
-
-        time = 8000,
-        xp = 20,
-        level = 2
-    },
-
     lockpick = {
         label = "Lockpick",
         item = "lockpick",
@@ -30,6 +15,6 @@ Config.Recipes = {
         time = 5000,
         xp = 10,
         level = 1
-    }
+    },
 
 }
